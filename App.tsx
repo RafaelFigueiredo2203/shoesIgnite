8ac2be3d93457c55b810b873c88e670eb1799b08
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-07-14 18:56:25 -0300

    Gerando build no android

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
-import { StatusBar } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from 'react-native';
 
+import { OneSignal } from 'react-native-onesignal';
 import { Routes } from './src/routes';
 
-import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { THEME } from './src/theme';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize('5a5202b5-373c-4090-8459-c0ec983dd346')
+OneSignal.Notifications.requestPermission(true)
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
