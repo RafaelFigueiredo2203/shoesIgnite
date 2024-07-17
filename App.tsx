a26badf535765467370dda23cf387984e1d11093
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-07-17 08:50:00 -0300

    Verificando clique na notificação em background

--- App.tsx
+++ App.tsx
@@ -2,12 +2,13 @@ import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/
 import { NativeBaseProvider } from 'native-base';
 import { StatusBar } from 'react-native';
 
-import { OneSignal } from 'react-native-onesignal';
+import { NotificationClickEvent, OneSignal } from 'react-native-onesignal';
 import { Routes } from './src/routes';
 
 import { Loading } from './src/components/Loading';
 import { THEME } from './src/theme';
 
+import { useEffect } from 'react';
 import { CartContextProvider } from './src/contexts/CartContext';
 import { tagUserInfoCreate } from './src/notifications/notificationsTags';
 
@@ -18,6 +19,16 @@ export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   tagUserInfoCreate("rafaelfigueiredojunior7@gmail.com")
+
+  useEffect(() => {
+    const handleNotificationClick = (event:NotificationClickEvent):void => {
+
+    }
+
+    OneSignal.Notifications.addEventListener("click", handleNotificationClick)
+  
+    return () => OneSignal.Notifications.removeEventListener("click",handleNotificationClick)
+  },[])
 
   return (
     <NativeBaseProvider theme={THEME}>
