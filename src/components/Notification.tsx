4872e0255d24c40bcc9289800bc954263a2621a0
RafaelFigueiredo2203 <rafaelfigueiredojunior7@gmail.com>
2024-07-17 08:41:42 -0300

    Enviando dados na notificação

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,27 +1,48 @@
-import { HStack, Text, IconButton, CloseIcon, Icon } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { CloseIcon, HStack, Icon, IconButton, Pressable, Text } from 'native-base';
+import { OSNotification } from 'react-native-onesignal';
 
 type Props = {
-  title: string;
+  data: OSNotification
   onClose: () => void;
 }
 
-export function Notification({ title, onClose }: Props) {
+type AdditionalDataProps = {
+  route?: string;
+  product_id?: string;
+}
+
+export function Notification({ data, onClose }: Props) {
+  const {navigate} = useNavigation()
+  
+  function handleOnPress(){
+    const {product_id,route} = data.additionalData as AdditionalDataProps
+    
+    if(route === 'details' && product_id){
+      navigate('details',{productId: product_id})
+      onClose()
+    }
+  }
+
   return (
+    <Pressable 
+    w="full" 
+    p={4} 
+    pt={12}
+    bgColor="gray.200"
+    position="absolute"
+    top={0}
+    onPress={handleOnPress}
+    >
     <HStack 
-      w="full" 
-      p={4} 
-      pt={12}
       justifyContent="space-between" 
       alignItems="center" 
-      bgColor="gray.200"
-      position="absolute"
-      top={0}
     >
         <Icon as={Ionicons} name="notifications-outline" size={5} color="black" mr={2}/>
 
         <Text fontSize="md" color="black" flex={1}>
-          {title}
+          {data.title}
         </Text>
 
       <IconButton 
@@ -33,5 +54,6 @@ export function Notification({ title, onClose }: Props) {
         onPress={onClose}
       />
     </HStack>
+    </Pressable>
   );
 }
